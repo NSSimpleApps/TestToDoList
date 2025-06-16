dcbdcec1c3ee9678b9af94abe0afbaf0c7c4080f
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2025-06-16 09:49:18 +0700

    Обновление.

--- TestToDoList/SceneDelegate.swift
+++ TestToDoList/SceneDelegate.swift
@@ -13,17 +13,19 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private let toDoListRouter = ToDoListRouter()
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-        
-        let window = UIWindow(windowScene: windowScene)
-        window.overrideUserInterfaceStyle = .dark
-        let loadingModule = self.toDoListRouter.createLoadingModule()
-        window.rootViewController = loadingModule
-        self.window = window
-        window.makeKeyAndVisible()
-        
-        self.toDoListRouter.createToDoListModule(comletion: { toDoListModule in
-            window.rootViewController = toDoListModule
-        })
+        if self.window == nil {
+            guard let windowScene = scene as? UIWindowScene else { return }
+            
+            let window = UIWindow(windowScene: windowScene)
+            window.overrideUserInterfaceStyle = .dark
+            let loadingModule = self.toDoListRouter.createLoadingModule()
+            window.rootViewController = loadingModule
+            self.window = window
+            window.makeKeyAndVisible()
+            
+            self.toDoListRouter.createToDoListModule(comletion: { toDoListModule in
+                window.rootViewController = toDoListModule
+            })
+        }
     }
 }
